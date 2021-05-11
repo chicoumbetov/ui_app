0d64c63d9ed593190ed50c9e4e7a0be611bbefd6
Amaury <amaury@web-premiere.fr>
2021-05-11 17:48:51 +0200

    ParametrerAjoutRevenu

--- screens/MesBiensScreen/Components/ParametrerAjoutRevenu.tsx
+++ screens/MesBiensScreen/Components/ParametrerAjoutRevenu.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import {
-  Button, Layout, Text, useTheme,
+  Button, Datepicker, Icon, Layout, Text, useTheme,
 } from '@ui-kitten/components';
 import {
-  StyleSheet, View,
+  StyleSheet, TouchableOpacity, View,
 } from 'react-native';
 
 import { useForm } from 'react-hook-form';
@@ -45,7 +45,10 @@ const ParametrerAjoutRevenu = () => {
 
   const [frequenceShow, setFrequenceShow] = useState(false);
   const [montantShow, setMontantShow] = useState(false);
+  const [revenuLoyer, setRevenuLoyer] = useState(false);
   const [dateDerniereEcheanceShow, setDateDerniereEcheanceShow] = useState(false);
+
+  const [montantValue, setMontantValue] = useState('Montant');
 
   /**
    *Variable pour gérer l'affichage des trois grandes partie
@@ -103,40 +106,57 @@ const ParametrerAjoutRevenu = () => {
 
             <View>
 
-              <Layout style={{ backgroundColor: 'transparent' }}>
+              <Layout style={{ backgroundColor: 'transparent', paddingBottom: 33 }}>
 
-                <Layout>
-                  <SelectComp name="typeRevenu" data={typeRevenu} placeholder="Type De Revenu" size="large" appearance="default" status="primary" />
+                <View>
+                  <SelectComp name="typeRevenu" data={typeRevenu} onChangeValue={(v) => { if (v === 'b1') { setRevenuLoyer(true); } else { setRevenuLoyer(false); } setMontantShow(true); setFrequenceShow(true); }} placeholder="Type De Revenu" size="large" appearance="default" status="primary" />
 
-                </Layout>
+                </View>
 
-                <Layout>
-                  <SelectComp name="Loyer" data={montant} placeholder="Montant" onChangeValue={(v) => { if (v === 'b1') { setMontantShow(true); } }} size="large" appearance="default" status="primary" />
+                <View>
 
-                </Layout>
+                  {montantShow && (
+                  <View>
+                    <TextInputComp name="montant" placeholder="Saisissez votre montant ici" />
 
-                {montantShow
-                  && (
-                  <Layout>
-                    <SelectComp name="typeMontant" data={typeMontant} placeholder="Type de Montant" size="large" onChangeValue={() => { setFrequenceShow(true); }} appearance="default" status="primary" />
-
-                  </Layout>
+                    {revenuLoyer && (
+                    <View>
+                      <TextInputComp name="charges" placeholder="Montant des charges" />
+                      <TextInputComp name="gestion" placeholder="Taux de frais de gestion" />
+                    </View>
+                    )}
+                  </View>
                   )}
-                {frequenceShow
-                  && (
-                  <>
-                    <Layout style={{ backgroundColor: 'transparent' }}>
-                      <SelectComp name="typeRevenu" data={frequence} placeholder="Fréquence" size="large" appearance="default" status="primary" />
-                      <TextInputComp style={{ marginBottom: 30 }} name="adresse" placeholder="Adresse" />
-                      <TextInputComp style={{ marginBottom: 30 }} name="complement" placeholder="Complément d'adresse" />
-                      <TextInputComp style={{ marginBottom: 30 }} name="codePostal" placeholder="Code Postal" />
-                      <TextInputComp style={{ marginBottom: 30 }} name="ville" placeholder="Ville" />
-                      <TextInputComp name="pays" placeholder="Pays" />
-                    </Layout>
-                  </>
-                  )}
+
+                </View>
+                <View>
+
+                  {frequenceShow
+                    && (
+                      <Layout style={{ backgroundColor: 'transparent' }}>
+                        <SelectComp name="typeRevenu" data={frequence} onChangeValue={() => setDateDerniereEcheanceShow(true)} placeholder="Fréquence" size="large" appearance="default" status="primary" />
+                        {dateDerniereEcheanceShow && (
+                          <Datepicker placeholder="Date de dernière échéance" />
+                        )}
+                      </Layout>
+
+                    )}
+                </View>
 
               </Layout>
+
+              {revenuLoyer && (
+              <View>
+                <Text style={{ paddingBottom: 30 }} category="h5">Ajouter un locataire</Text>
+                <TextInputComp style={{ paddingBottom: 30 }} name="charges" placeholder="Montant des charges" />
+                <TextInputComp style={{ paddingBottom: 30 }} name="charges" placeholder="Montant des charges" />
+                <TextInputComp style={{ paddingBottom: 30 }} name="charges" placeholder="Montant des charges" />
+                <Text style={{ paddingBottom: 30 }} category="h5">Date de début de bail</Text>
+                <Datepicker style={{ paddingBottom: 30 }} />
+                <Text style={{ paddingBottom: 30 }} category="h5">Date de fin de bail</Text>
+                <Datepicker style={{ paddingBottom: 30 }} />
+              </View>
+              )}
 
             </View>
 
@@ -166,5 +186,37 @@ const styles = StyleSheet.create({
     marginTop: 12,
     paddingVertical: 25,
     paddingHorizontal: 26,
+  },
+  headerDown: {
+    padding: 22,
+    marginBottom: 36,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    borderRadius: 7,
+    backgroundColor: '#37a3de',
+    shadowColor: 'rgba(199, 199, 199, 0.5)',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowRadius: 4,
+    shadowOpacity: 1,
+  },
+  headerUp: {
+    padding: 22,
+    marginBottom: 15,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    borderRadius: 7,
+    backgroundColor: '#5fc4ee',
+    shadowColor: 'rgba(199, 199, 199, 0.5)',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowRadius: 4,
+    shadowOpacity: 1,
   },
 });

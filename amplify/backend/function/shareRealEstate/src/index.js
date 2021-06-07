900d89e2cd2bf72994e67f5dd5c4a89e0064ee98
Amaury <amaury@web-premiere.fr>
2021-06-07 18:08:00 +0200

    Function email et ajouter un ultilisateur au bien

--- amplify/backend/function/shareRealEstate/src/index.js
+++ amplify/backend/function/shareRealEstate/src/index.js
@@ -1,11 +1,9 @@
 exports.handler = (event) => {
   //eslint-disable-line
-  console.log(JSON.stringify(event, null, 2));
   event.Records.forEach((record) => {
-    console.log(record.eventID);
-    console.log(record);
-    console.log(record.eventName);
-    console.log('DynamoDB Record: %j', record.dynamodb);
+    if (record.eventName === 'INSERT') {
+      console.log(record.dynamodb.NewImage.email.S);
+    }
   });
   return Promise.resolve('Successfully processed DynamoDB record');
 };

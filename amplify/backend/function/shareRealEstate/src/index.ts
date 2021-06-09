adfaf8d5bb78cd9b675d148ae77877985027e20e
David-Julian Buch <david@web-premiere.fr>
2021-06-09 21:29:48 +0200

    MAJ Amplify

--- amplify/backend/function/shareRealEstate/src/index.ts
+++ amplify/backend/function/shareRealEstate/src/index.ts
@@ -9,7 +9,7 @@ Amplify Params - DO NOT EDIT */
 
 import { getUserByEmail } from '/opt/nodejs/src/UserQueries';
 import getAppSyncClient from '/opt/nodejs/src/AppSyncClient';
-import { updateRealEstateMutation, getRealEstate  } from '/opt/nodejs/src/RealEstateMutation';
+import { updateRealEstateMutation, getRealEstate } from '/opt/nodejs/src/RealEstateMutation';
 import { sendEmail } from '/opt/nodejs/src/SendMail';
 
 exports.handler = async (event) => {
@@ -23,14 +23,14 @@ exports.handler = async (event) => {
     // so, it will immediately continue.
     await promise;
     if (record.eventName === 'INSERT') {
-      const {email, type, realEstateId} = record.dynamodb.NewImage;
+      const { email, type, realEstateId } = record.dynamodb.NewImage;
       const user = await getUserByEmail(appSyncClient, email.S);
-      const realEstate = await getRealEstate(appSyncClient, realEstateId.S)
+      const realEstate = await getRealEstate(appSyncClient, realEstateId.S);
       if (user && realEstate) {
         if (type.S === 'Admin') {
-          const admins = realEstate.admins;
-          const exists = admins.find((admin) =>{
-            if (admin === user.id){
+          const { admins } = realEstate;
+          const exists = admins.find((admin) => {
+            if (admin === user.id) {
               return true;
             }
             return false;
@@ -43,18 +43,18 @@ exports.handler = async (event) => {
             variables: {
               input: {
                 id: realEstateId.S,
-                admins
+                admins,
               },
             },
           });
         } else {
           const shared = realEstate.shared || [];
-          const exists = shared.find((share) =>{
-            if (share === user.id){
+          const exists = shared.find((share) => {
+            if (share === user.id) {
               return true;
             }
-            return false
-          })
+            return false;
+          });
           if (exists === undefined) {
             shared.push(user.id);
           }
@@ -62,7 +62,7 @@ exports.handler = async (event) => {
             variables: {
               input: {
                 id: record.dynamodb.NewImage.realEstateId.S,
-                shared
+                shared,
               },
             },
           });

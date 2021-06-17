866ab3f1ed6a3b065f4c76efaaac37ea0ea079d6
Amaury <amaury@web-premiere.fr>
2021-06-17 11:34:39 +0200

    querie pendinginvitation

---
+++ amplify/backend/function/graphqllayer/lib/nodejs/src/PendingInvitationQueries.ts
@@ -0,0 +1,54 @@
+import gql from 'graphql-tag';
+import {
+  PendingInvitationByEmailQuery,
+  PendingInvitationByEmailQueryVariables,
+} from '../../../../../../../src/API';
+import { AppSyncClient } from './AppSyncClient';
+
+const listPendingInvitationByEmail = async (client: AppSyncClient, pendingEmail: string) => {
+  try {
+    const { data } = await client.query<
+    PendingInvitationByEmailQuery,
+    PendingInvitationByEmailQueryVariables
+    >({
+      query: gql(`query PendingInvitationByEmail(
+    $email: String
+  ) {
+    pendingInvitationByEmail(
+      email: $email
+    ) {
+      items {
+        id
+        realEstateId
+        email
+        type
+        _version
+        _deleted
+        _lastChangedAt
+        createdAt
+        updatedAt
+       
+        
+      }
+      nextToken
+      startedAt
+    }
+  }`), // use your graphql query here
+      variables: {
+        email: pendingEmail,
+      },
+      fetchPolicy: 'no-cache',
+    });
+    if (data.pendingInvitationByEmail.items.length > 0) {
+      return data.pendingInvitationByEmail.items;
+    }
+    return false;
+  } catch (e) {
+    console.error(e);
+    return false;
+  }
+};
+
+export {
+  listPendingInvitationByEmail,
+};
--- amplify/backend/function/waitingUser/src/index.ts
+++ amplify/backend/function/waitingUser/src/index.ts
@@ -1,10 +1,21 @@
-exports.handler = event => {
+/* Amplify Params - DO NOT EDIT
+	API_OMEDOMREST_APIID
+	API_OMEDOMREST_APINAME
+	API_OMEDOM_GRAPHQLAPIENDPOINTOUTPUT
+	API_OMEDOM_GRAPHQLAPIIDOUTPUT
+	ENV
+	REGION
+Amplify Params - DO NOT EDIT */
+
+import { getUserByEmail } from '/opt/nodejs/src/UserQueries';
+import getAppSyncClient from '/opt/nodejs/src/AppSyncClient';
+import { updateRealEstateMutation, getRealEstate } from '/opt/nodejs/src/RealEstateMutation';
+import { sendTemplateEmail } from '/opt/nodejs/src/SendMail';
+
+exports.handler = async (event) => {
   //eslint-disable-line
   console.log(JSON.stringify(event, null, 2));
-  event.Records.forEach(record => {
-    console.log(record.eventID);
-    console.log(record.eventName);
-    console.log('DynamoDB Record: %j', record.dynamodb);
-  });
+  const appSyncClient = getAppSyncClient(process.env);
+
   return Promise.resolve('Successfully processed DynamoDB record');
 };

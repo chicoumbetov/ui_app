3f05e37deafe830b23525c38287d9cd7c78b019d
Shynggys Umbetov <shynggys@web-premiere.fr>
2021-06-17 12:16:25 +0200

    Charge pass props through 3 charges components, dinamic change of date range

---
+++ amplify/.temp/#current-cloud-backend/function/budgetinsightlayer/lib/nodejs/src/BIApiKeys.ts
@@ -0,0 +1,14 @@
+const BIApiKeys = {
+  dev: {
+    clientId: '45913022',
+    clientSecret: 'EuDtfrmKj82rd5Wt3ICk5wlDlMJYuV/Y',
+    domain: 'omedom1-sandbox',
+  },
+  prod: {
+    clientId: '_____',
+    clientSecret: '_____',
+    domain: '-----',
+  },
+};
+
+export default BIApiKeys;

72d038bfa71057159eb2e5d344b64cdc6303dd5b
Shynggys Umbetov <shynggys@web-premiere.fr>
2021-06-23 17:07:42 +0200

    Graphic II css

--- components/Graphics/GraphicsII.tsx
+++ components/Graphics/GraphicsII.tsx
@@ -1,8 +1,46 @@
 import React from 'react';
 import { View } from 'react-native';
 import { Text, useTheme } from '@ui-kitten/components';
-import { VictoryBar, VictoryChart, VictoryLine } from 'victory-native';
+import {
+  VictoryArea, VictoryAxis, VictoryBar, VictoryChart, VictoryLabel, VictoryLine,
+} from 'victory-native';
 import MaxWidthContainer from '../MaxWidthContainer';
+
+const entree = [
+  { x: 'janvier', y: 500 },
+  { x: 'fevrier', y: 700 },
+  { x: 'mars', y: 1000 },
+  { x: 'avril', y: 400 },
+  { x: 'mai', y: 300 },
+  { x: 'juin', y: 100 },
+];
+const sortie = [
+  { x: 'avril', y: 600 },
+  { x: 'mai', y: 500 },
+  { x: 'juin', y: 700 },
+];
+
+const delta = [
+  { x: 'janvier', y: 500 },
+  { x: 'fevrier', y: 700 },
+  { x: 'mars', y: 1000 },
+  { x: 'avril', y: 600 },
+  { x: 'mai', y: 500 },
+  { x: 'juin', y: 700 },
+  { x: 'juillet', y: 1000 },
+  { x: 'aout', y: 600 },
+];
+
+const evolution = [
+  { x: 'janvier', y: 200 },
+  { x: 'fevrier', y: 500 },
+  { x: 'mars', y: -300 },
+  { x: 'avril', y: 1200 },
+  { x: 'mai', y: 600 },
+  { x: 'juin', y: -300 },
+  { x: 'juillet', y: 200 },
+  { x: 'aout', y: 100 },
+];
 
 const GraphicsII = () => {
   const theme = useTheme();
@@ -18,77 +56,89 @@ const GraphicsII = () => {
     >
 
       <VictoryChart
-        domain={{ x: [0.5, 5.5], y: [0, 10] }}
-        domainPadding={{ x: 100 }}
+        height={1000}
+        // maxDomain={{ y: 0 }}
+        // minDomain={{ y: 0 }}
+        animate={{
+          duration: 6000,
+          onLoad: { duration: 1000 },
+        }}
+        /* eslint-disable-next-line max-len */
+        // categories={{ x: [ 'janvier', 'fevrier', 'mars', 'avril', 'mai', 'juin', 'juillet', 'aout', 'septembre', 'octobre', 'novembre', 'decembre'], }}
         padding={{
-          top: 40, bottom: 80, left: 40, right: 80,
+          top: 20, bottom: 10, left: 90, right: 40,
         }}
       >
-        <VictoryLine
+        <VictoryAxis
+          // theme={{ }}
+          crossAxis
+          standalone
+          // offsetX={200}
+          tickLabelComponent={<VictoryLabel style={{ fontSize: '30px', fill: '#b5b5b5' }} />}
+        />
+        <VictoryAxis
+          dependentAxis
+          crossAxis
+          standalone={false}
+          // label="occurrences"
+          tickLabelComponent={<VictoryLabel style={{ fontSize: '30px', fill: '#b5b5b5' }} />}
           style={{
-            data: { stroke: '#c43a31' },
+            // axis: { stroke: '#b5b5b5' },
+            grid: { stroke: '#b5b5b5' },
+          }}
+        />
+        <VictoryLine
+          labelComponent={(
+            <VictoryLabel
+              style={[
+                { fill: 'black', fontSize: 16, fontFamily: 'confortaa_SemiBold' },
+                { fill: '#b5b5b5', fontSize: 16, fontFamily: 'confortaa_SemiBold' },
+              ]}
+            />
+            )}
+          style={{
+            data: { stroke: 'green' },
             parent: { border: '1px solid #ccc' },
           }}
-          interpolation="natural"
-          height={250}
+          // interpolation="natural"
           width={250}
+          data={delta}
+        />
+        <VictoryBar
+          barWidth={40}
+          alignment="start"
           animate={{
-            duration: 6000,
-            onLoad: { duration: 1000 },
+            onExit: {
+              duration: 500,
+              before: () => ({ opacity: 0.3, _y: 0 }),
+            },
+            onEnter: {
+              duration: 500,
+              before: () => ({ opacity: 0.3, _y: 0 }),
+              // eslint-disable-next-line no-underscore-dangle
+              after: (datum) => ({ opacity: 1, _y: datum._y }),
+            },
           }}
-          categories={{
-            x: ['birds', 'cats', 'dogs', 'fish'],
-          }}
-          data={[
-            {
-              x: 1, y: 1, label: 'first', symbol: 'star', opacity: 0.5, fill: 'blue',
-            },
-            {
-              x: 2, y: 2, label: 'second', symbol: 'circle', opacity: 0.8, fill: 'red',
-            },
-            {
-              x: 3, y: 3, label: 'third', symbol: 'square', fill: 'gold',
-            },
-            {
-              x: 4, y: 4, label: 'fourth', symbol: 'diamond', fill: 'green',
-            },
-          ]}
+          style={{ data: { fill: theme['color-danger-500'] } }}
+          data={entree}
         />
         <VictoryBar
-          height={250}
-          barWidth={20}
-          data={[
-            {
-              x: 1, y: 1, label: 'first', symbol: 'star', opacity: 0.5, fill: 'blue',
-            },
-            {
-              x: 2, y: 2, label: 'second', symbol: 'circle', opacity: 0.8, fill: 'red',
-            },
-            {
-              x: 3, y: 3, label: 'third', symbol: 'square', fill: 'gold',
-            },
-            {
-              x: 4, y: 4, label: 'fourth', symbol: 'diamond', fill: 'green',
-            },
-          ]}
+          barWidth={40}
+          alignment="end"
+          style={{ data: { fill: theme['color-info-400'] } }}
+          data={sortie}
         />
-        <VictoryBar
-          height={250}
-          barWidth={20}
-          data={[
-            {
-              x: 4, y: 1, label: 'yep', symbol: 'star', opacity: 0.5, fill: 'yellow',
+        <VictoryArea
+          style={{
+            data: {
+              fill: theme['color-warning-500'], stroke: theme['color-warning-500'], fillOpacity: 0.4, strokeWidth: 3,
             },
-            {
-              x: 3, y: 2, label: 'yo', symbol: 'circle', opacity: 0.8, fill: 'red',
-            },
-            {
-              x: 2, y: 3, label: 'the', symbol: 'square', fill: 'gold',
-            },
-            {
-              x: 1, y: 4, label: 'air', symbol: 'diamond', fill: 'green',
-            },
-          ]}
+            parent: { border: '1px solid #ccc' },
+          }}
+          // interpolation="natural"
+          width={250}
+            // categories={{x: ['birds', 'cats', 'dogs', 'fish'],}}
+          data={evolution}
         />
       </VictoryChart>
 
